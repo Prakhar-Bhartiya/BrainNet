01986d3e6835327abd652d33bc3e453a066af888
Shawn Deason <61474308+smdeason@users.noreply.github.com>
2022-04-24 12:56:36 -0700

    Created basic layout

--- Android/app/src/main/java/com/example/livebrain/MainActivity.java
+++ Android/app/src/main/java/com/example/livebrain/MainActivity.java
@@ -2,19 +2,14 @@ package com.example.livebrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.Spinner;
 
-import com.chaquo.python.PyException;
-import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
@@ -25,31 +20,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Connect ui to code
+        Button btnFetchUsers = findViewById(R.id.btnFetchUsers);
+        Button btnSelectFeature = findViewById(R.id.btnSelectFeature);
+        Button btnRunModels = findViewById(R.id.btnRunModels);
+        Spinner featuresSpinner = findViewById(R.id.spinnerFeatures);
+        EditText editTextUserSelect = findViewById(R.id.editTextUsers);
+
+        ArrayAdapter<CharSequence> featuresAdapter = ArrayAdapter.createFromResource(this, R.array.features_array, android.R.layout.simple_spinner_item);
+        featuresAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        featuresSpinner.setAdapter(featuresAdapter);
+        featuresSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
         if(!Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
         }
 
         Python py = Python.getInstance();
-        PyObject module = py.getModule("plot");
 
-        Button plotBtn = findViewById(R.id.button);
-        EditText etX = findViewById(R.id.etX);
-        EditText etY = findViewById(R.id.etY);
-        ImageView imageOut = findViewById(R.id.imageView);
-        plotBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    byte[] output = module.callAttr("plot", etX.getText().toString(), etY.getText().toString()).toJava(byte[].class);
-                    Bitmap image = BitmapFactory.decodeByteArray(output, 0, output.length);
-                    imageOut.setImageBitmap(image);
-
-                    InputMethodManager keyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    keyboard.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-                } catch(PyException e) {
-                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
-        });
     }
 }
